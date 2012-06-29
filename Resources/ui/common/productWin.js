8517bce2b9bb0e633d27294c0decdb75bf2c2bcf
Hazem Khaled <hazem.khaled@gmail.com>
2012-06-29 18:07:00 +0200

    image reduse, product + & -

--- Resources/ui/common/productWin.js
+++ Resources/ui/common/productWin.js
@@ -7,13 +7,14 @@ function productWin(product) {
 	var mainScroll = Ti.UI.createScrollView();
 	self.add(mainScroll);
 
-	var picWidth = Ti.Platform.displayCaps.platformWidth - 40;
+	//var picWidth = Ti.Platform.displayCaps.platformWidth - 40;
 	var productImg = Ti.UI.createImageView({
-		image : Ti.App.APIURL + 'api/pic/product/' + product.id + '/123/' + picWidth + '/1',
-		width : picWidth + 'dp',
-		height : '123dp',
-		top : '34dp',
-		zIndex : 99
+		image : Ti.App.APIURL + 'api/pic/product/' + product.id + '/90/90/1',
+		width : '90dp',
+		height : '90dp',
+		top : '10dp',
+		right : '10dp',
+		borderRadius : 45
 	});
 
 	productImg.addEventListener('click', function() {
@@ -30,13 +31,93 @@ function productWin(product) {
 	// description
 	var descLbl = Ti.UI.createLabel({
 		text : product.desc,
-		height : 'auto',
-		align : 'right',
-		width : Ti.Platform.osname == 'android' ? '100%' : picWidth + 'dp',
-		top : '250dp',
-		color : '#000000'
+		textAlign : 'right',
+		color : '#000000',
+		top : '20dp',
+		right : '110dp',
+		left : '10dp'
 	});
 	mainScroll.add(descLbl);
+
+	// + and - buttons with product quantity
+	function getQuantityFieldValue() {
+		return parseInt(quantityField.getValue());
+	}
+
+	function setQuantityFieldValue(newValue) {
+		var value = parseInt(newValue);
+
+		// is it in minus ?
+		if (value < 0) {
+			value = 0;
+		}
+
+		// is it more then inventory quantity
+		if (value > parseInt(product.quantity)) {
+
+			Ti.UI.createAlertDialog({
+				title : 'الكمية لا تكفي',
+				message : 'متوفر الان فقط ' + product.quantity + ' وحدة من هذا المنتج',
+				cancel : 0,
+				buttonNames : ['موافق']
+			}).show();
+
+			value = parseInt(product.quantity);
+		}
+
+		quantityField.setValue(value);
+	}
+
+	var quantityField = Ti.UI.createTextField({
+		value : '0',
+		textAlign : 'center',
+		width : '50dp',
+		height : '40dp',
+		left : '10dp',
+		top : '70dp',
+		editable : false,
+		enabled : false
+	});
+
+	quantityField.addEventListener('change', function(e) {
+		setQuantityFieldValue(getQuantityFieldValue());
+	});
+
+	self.add(quantityField);
+
+	var quantityMinusBtn = Ti.UI.createButton({
+		title : '-',
+		textAlign : 'center',
+		width : '40dp',
+		height : '40dp',
+		left : '65dp',
+		top : '70dp'
+	});
+	self.add(quantityMinusBtn);
+
+	var quantityPlusBtn = Ti.UI.createButton({
+		title : '+',
+		textAlign : 'center',
+		width : '40dp',
+		height : '40dp',
+		left : '110dp',
+		top : '70dp'
+	});
+	self.add(quantityPlusBtn);
+
+	function plusMinusBtnsOnClick(e) {
+
+		if (e.source.title == '+') {
+
+			setQuantityFieldValue(getQuantityFieldValue() + 1);
+		} else {
+			setQuantityFieldValue(getQuantityFieldValue() - 1);
+		}
+	}
+
+
+	quantityMinusBtn.addEventListener('click', plusMinusBtnsOnClick);
+	quantityPlusBtn.addEventListener('click', plusMinusBtnsOnClick);
 
 	return self;
 };
