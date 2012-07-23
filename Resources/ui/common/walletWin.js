09d9a96458992f52ded420e79da3c2e80a01fc57
Hazem Khaled <hazem.khaled@gmail.com>
2012-07-23 05:01:26 +0200

    declare variables before use it, by using var, make declaration on the level you want to use this variable

--- Resources/ui/common/walletWin.js
+++ Resources/ui/common/walletWin.js
@@ -1,175 +1,175 @@
 function walletWin() {
 
-	var self, androidshift = 0, loadinLabel, chargeBtn, balancCurr, img, loginBtn, msgLabel;
+    var self, androidshift = 0, loadinLabel, chargeBtn, balancCurr, img, loginBtn, msgLabel, auth = require('/lib/auth'), balanceTitle;
 
-	self = Ti.UI.createWindow({
-		title : 'المحفظة',
-		backgroundImage : 'images/common/bg.jpg',
-		barImage : 'images/common/Navigation_Bar.jpg',
-		barColor : '#d3d3d3'
-	});
+    self = Ti.UI.createWindow({
+        title : 'المحفظة',
+        backgroundImage : 'images/common/bg.jpg',
+        barImage : 'images/common/Navigation_Bar.jpg',
+        barColor : '#d3d3d3'
+    });
 
-	Ti.App.addEventListener('showWalletAfterLogin', function() {
+    Ti.App.addEventListener('showWalletAfterLogin', function() {
 
-		loadinLabel = Ti.UI.createLabel({
-			text : 'جاري التحميل ....',
-			left : 0,
-			right : '10dp',
-			top : '10dp',
-			color : '#000000',
-			textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT
-		});
-		self.add(loadinLabel);
+        var loadinLabel, chargeBtn, img, balanceLbl, balancCurr;
 
-		function getBalance() {
+        loadinLabel = Ti.UI.createLabel({
+            text : 'جاري التحميل ....',
+            left : 0,
+            right : '10dp',
+            top : '10dp',
+            color : '#000000',
+            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT
+        });
+        self.add(loadinLabel);
 
-			var xhr = Ti.Network.createHTTPClient();
+        function getBalance() {
 
-			xhr.open('GET', Ti.App.APIURL + 'api/walletBalance/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency'));
+            var xhr = Ti.Network.createHTTPClient();
 
-			xhr.onerror = function() {
+            xhr.open('GET', Ti.App.APIURL + 'api/walletBalance/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency'));
 
-				loadinLabel.setText('لا يوجد نتائج هنا في الوقت الحالي !!');
-			};
+            xhr.onerror = function() {
 
-			xhr.onload = function() {
+                loadinLabel.setText('لا يوجد نتائج هنا في الوقت الحالي !!');
+            };
 
-				var row = JSON.parse(this.responseText);
-				balanceLbl.text = row.balance;
-				loadinLabel.setVisible(false);
-				balanceTitle.setVisible(true);
-				balanceLbl.setVisible(true);
-				balancCurr.setVisible(true);
-			};
+            xhr.onload = function() {
 
-			xhr.send();
-		}
+                var row = JSON.parse(this.responseText);
+                balanceLbl.setText(row.balance);
+                loadinLabel.setVisible(false);
+                balanceTitle.setVisible(true);
+                balanceLbl.setVisible(true);
+                balancCurr.setVisible(true);
+            };
 
-		chargeBtn = Ti.UI.createButton({
-			title : 'شحن المحفظة',
-			top : '200dp',
-			right : '1%',
-			left : '1%',
-			width : '98%',
-			color : '#000000',
-			backgroundImage : 'images/common/button_ok.png'
-		});
+            xhr.send();
+        }
 
-		chargeBtn.addEventListener('click', function() {
-			Ti.App.fireEvent('openChargeWalletWindow');
-		});
+        chargeBtn = Ti.UI.createButton({
+            title : 'شحن المحفظة',
+            top : '200dp',
+            right : '1%',
+            left : '1%',
+            width : '98%',
+            color : '#000000',
+            backgroundImage : 'images/common/button_ok.png'
+        });
 
-		self.add(chargeBtn);
+        chargeBtn.addEventListener('click', function() {
+            Ti.App.fireEvent('openChargeWalletWindow');
+        });
 
-		img = Ti.UI.createImageView({
-			image : 'images/common/wallet_balance.png',
-			width : '144dp',
-			height : '166dp',
-			right : '100dp',
-			top : '20px'
-		});
-		self.add(img);
+        self.add(chargeBtn);
 
-		balanceTitle = Ti.UI.createLabel({
-			text : 'الرصيد',
-			left : 0,
-			right : '150dp',
-			top : '60dp',
-			color : '#000000',
-			visible : false,
-			textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-			font : {
-				fontFamily : 'Arial',
-				fontSize : '16dp',
-				fontWeight : 'bold'
-			}
-		});
-		self.add(balanceTitle);
+        img = Ti.UI.createImageView({
+            image : 'images/common/wallet_balance.png',
+            width : '144dp',
+            height : '166dp',
+            right : '100dp',
+            top : '20px'
+        });
+        self.add(img);
 
-		balanceLbl = Ti.UI.createLabel({
-			text : '',
-			left : 0,
-			right : '155dp',
-			top : '85dp',
-			color : '#000000',
-			visible : false,
-			textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-			color : 'red',
-			font : {
-				fontFamily : 'Arial',
-				fontSize : '20dp',
-				fontWeight : 'bold'
-			}
-		});
-		Ti.App.balanceLbl = balanceLbl;
-		self.add(Ti.App.balanceLbl);
+        balanceTitle = Ti.UI.createLabel({
+            text : 'الرصيد',
+            left : 0,
+            right : '150dp',
+            top : '60dp',
+            color : '#000000',
+            visible : false,
+            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+            font : {
+                fontFamily : 'Arial',
+                fontSize : '16dp',
+                fontWeight : 'bold'
+            }
+        });
+        self.add(balanceTitle);
 
-		balancCurr = Ti.UI.createLabel({
-			text : Ti.App.Properties.getString('currencyName'),
-			left : 0,
-			right : '127dp',
-			top : '110dp',
-			color : '#000000',
-			visible : false,
-			textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-			font : {
-				fontFamily : 'Arial',
-				fontSize : '15dp',
-				fontWeight : 'bold'
-			}
-		});
-		self.add(balancCurr);
+        balanceLbl = Ti.UI.createLabel({
+            text : '',
+            left : 0,
+            right : '155dp',
+            top : '85dp',
+            visible : false,
+            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+            color : 'red',
+            font : {
+                fontFamily : 'Arial',
+                fontSize : '20dp',
+                fontWeight : 'bold'
+            }
+        });
+        Ti.App.balanceLbl = balanceLbl;
+        self.add(Ti.App.balanceLbl);
 
-		getBalance();
+        balancCurr = Ti.UI.createLabel({
+            text : Ti.App.Properties.getString('currencyName'),
+            left : 0,
+            right : '127dp',
+            top : '110dp',
+            color : '#000000',
+            visible : false,
+            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+            font : {
+                fontFamily : 'Arial',
+                fontSize : '15dp',
+                fontWeight : 'bold'
+            }
+        });
+        self.add(balancCurr);
 
-		self.remove(loginBtn);
-		self.remove(msgLabel);
-	});
+        getBalance();
 
-	Ti.App.addEventListener('showWalletBeforLogin', function() {
+        self.remove(loginBtn);
+        self.remove(msgLabel);
+    });
 
-		msgLabel = Ti.UI.createLabel({
-			text : 'يرجى تسجيل الدخول',
-			width : '100%',
-			top : (40 + androidshift) + 'dp',
-			color : '#ffffff',
-			textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-			font : {
-				fontFamily : 'Arial',
-				fontSize : '17dp',
-				fontWeight : 'bold'
-			}
-		});
-		self.add(msgLabel);
+    Ti.App.addEventListener('showWalletBeforLogin', function() {
 
-		loginBtn = auth.loginBtn(Ti.UI.createButton({
-			title : 'تسجيل دخول',
-			top : (100 + androidshift) + 'dp',
-			backgroundImage : 'images/common/button_ok.png',
-			paddingLeft : '5dp',
-			paddingRight : '5dp',
-			color : '#000000'
-		}));
-		self.add(loginBtn);
+        msgLabel = Ti.UI.createLabel({
+            text : 'يرجى تسجيل الدخول',
+            width : '100%',
+            top : (40 + androidshift) + 'dp',
+            color : '#ffffff',
+            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+            font : {
+                fontFamily : 'Arial',
+                fontSize : '17dp',
+                fontWeight : 'bold'
+            }
+        });
+        self.add(msgLabel);
 
-		self.remove(balancCurr);
-		self.remove(Ti.App.balanceLbl);
-		self.remove(balanceTitle);
-		self.remove(img);
-		self.remove(loadinLabel);
-		self.remove(chargeBtn);
-	});
+        loginBtn = auth.loginBtn(Ti.UI.createButton({
+            title : 'تسجيل دخول',
+            top : (100 + androidshift) + 'dp',
+            backgroundImage : 'images/common/button_ok.png',
+            paddingLeft : '5dp',
+            paddingRight : '5dp',
+            color : '#000000'
+        }));
+        self.add(loginBtn);
 
-	var auth = require('/lib/auth');
-	if (!auth.isLogedIn()) {
+        self.remove(balancCurr);
+        self.remove(Ti.App.balanceLbl);
+        self.remove(balanceTitle);
+        self.remove(img);
+        self.remove(loadinLabel);
+        self.remove(chargeBtn);
+    });
 
-		Ti.App.fireEvent('showWalletBeforLogin');
-	} else {
+    if (!auth.isLogedIn()) {
 
-		Ti.App.fireEvent('showWalletAfterLogin');
-	}
+        Ti.App.fireEvent('showWalletBeforLogin');
+    } else {
 
-	return self;
+        Ti.App.fireEvent('showWalletAfterLogin');
+    }
+
+    return self;
 }
 
 module.exports = walletWin;
