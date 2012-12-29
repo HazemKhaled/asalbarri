732808a12be4c10d446a09ba370481f0ffb05fe2
Hazem Khaled <hazem.khaled@gmail.com>
2012-12-30 01:58:56 +0200

    some fixes

--- Resources/ui/common/myOrdersWin.js
+++ Resources/ui/common/myOrdersWin.js
@@ -1,285 +1,289 @@
 function myOrdersWin() {
 
-    var self, tableView, auth = require('/lib/auth'), rows, i, row, dateTitleLbl, dateLbl, totaltitleLbl, priceLbl, statusTitleLbl, statustxt = '', statusLbl, loginBtn, msgLabel, submitBtn;
+	var self, tableView, auth = require('/lib/auth'), rows, i, row, dateTitleLbl, dateLbl, totaltitleLbl, priceLbl, statusTitleLbl, statustxt = '', statusLbl, loginBtn, msgLabel, submitBtn;
 
-    self = Ti.UI.createWindow({
-        title : 'طلباتي',
-        backgroundImage : '/images/bg.jpg',
-        barImage : '/images/Navigation_Bar.jpg',
-        barColor : 'gray'
-    });
+	self = Ti.UI.createWindow({
+		title : 'طلباتي',
+		backgroundImage : '/images/bg.jpg',
+		barImage : '/images/Navigation_Bar.jpg',
+		barColor : 'gray'
+	});
 
-    Ti.App.addEventListener('showMyordersAfterLogin', function() {
+	Ti.App.addEventListener('showMyordersAfterLogin', function() {
 
-        function filterData() {
+		function filterData() {
 
-            tableView.fireEvent('runLoading');
+			tableView.fireEvent('runLoading');
 
-            var tableRows = [], xhr;
+			var tableRows = [], xhr;
 
-            xhr = Ti.Network.createHTTPClient();
+			xhr = Ti.Network.createHTTPClient();
 
-            xhr.open('GET', Ti.App.APIURL + 'api/ordersByUserID/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency', 1));
+			xhr.open('GET', Ti.App.APIURL + 'api/ordersByUserID/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency', 1));
 
-            xhr.onerror = function() {
-                tableView.fireEvent('reloadData', {
-                    rows : []
-                });
-            };
+			xhr.onerror = function() {
+				tableView.fireEvent('reloadData', {
+					rows : []
+				});
+			};
 
-            xhr.onload = function() {
-                try {
-                    rows = JSON.parse(this.responseText);
-                } catch (e) {
+			xhr.onload = function() {
+				try {
+					rows = JSON.parse(this.responseText);
+				} catch (e) {
 
-                    tableView.fireEvent('reloadData', {
-                        rows : []
-                    });
-                    return false;
-                }
+					tableView.fireEvent('reloadData', {
+						rows : []
+					});
+					return false;
+				}
 
-                for (i in rows ) {
-                    if (rows.hasOwnProperty(i)) {
-                        row = Ti.UI.createTableViewRow({
-                            height : 95,
-                            myTitle : rows[i].title,
-                            data : rows[i],
-                            className : 'myOrderRow',
-                            backgroundImage : '/images/TableViewRowBG.jpg',
-                            selectedBackgroundImage : '/images/TableViewRowSelectedBG.png'
-                        });
+				for (i in rows ) {
+					if (rows.hasOwnProperty(i)) {
+						row = Ti.UI.createTableViewRow({
+							height : 95,
+							myTitle : rows[i].title,
+							data : rows[i],
+							className : 'myOrderRow',
+							backgroundImage : '/images/TableViewRowBG.jpg',
+							selectedBackgroundImage : '/images/TableViewRowSelectedBG.png'
+						});
 
-                        var arrow = Ti.UI.createImageView({
-                            image : '/images/icon_7.png',
-                            top : 6,
-                            left : 0,
-                            width : 23,
-                            height : 79
-                        });
-                        row.add(arrow);
+						var arrow = Ti.UI.createImageView({
+							image : '/images/icon_7.png',
+							top : 6,
+							left : 0,
+							width : 23,
+							height : 79
+						});
+						row.add(arrow);
 
-                        totaltitleLbl = Ti.UI.createLabel({
-                            text : 'الاجمالي : ',
-                            right : 10,
-                            top : 10,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-                            color : '#ffffff',
-                            font : {
-                                fontFamily : 'Arial',
-                                fontSize : 17,
-                                fontWeight : 'bold'
-                            }
-                        });
-                        row.add(totaltitleLbl);
+						totaltitleLbl = Ti.UI.createLabel({
+							text : 'الاجمالي : ',
+							right : 10,
+							top : 10,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+							color : '#ffffff',
+							font : {
+								fontFamily : 'Arial',
+								fontSize : 17,
+								fontWeight : 'bold'
+							}
+						});
+						row.add(totaltitleLbl);
 
-                        priceLbl = Ti.UI.createLabel({
-                            text : rows[i].total_price + ' ' + Ti.App.Properties.getString('currencyName', 'دولار أمريكي'),
-                            right : 85,
-                            top : 10,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-                            color : '#ffffff',
-                            font : {
-                                fontFamily : 'Arial',
-                                fontSize : 17,
-                                fontWeight : 'bold'
-                            }
-                        });
-                        row.add(priceLbl);
+						priceLbl = Ti.UI.createLabel({
+							text : rows[i].total_price + ' ' + Ti.App.Properties.getString('currencyName', 'دولار أمريكي'),
+							right : 85,
+							top : 10,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+							color : '#ffffff',
+							font : {
+								fontFamily : 'Arial',
+								fontSize : 17,
+								fontWeight : 'bold'
+							}
+						});
+						row.add(priceLbl);
 
-                        var datebackground = Ti.UI.createImageView({
-                            image : '/images/bg_time.png',
-                            top : 38,
-                            right : 0,
-                            width : 239,
-                            height : 22
-                        });
-                        row.add(datebackground);
+						var datebackground = Ti.UI.createImageView({
+							image : '/images/bg_time.png',
+							top : 38,
+							right : 0,
+							width : 239,
+							height : 22
+						});
+						row.add(datebackground);
 
-                        dateTitleLbl = Ti.UI.createLabel({
-                            text : 'التاريخ : ',
-                            left : 0,
-                            right : 10,
-                            top : 38,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-                            color : '#ffffff',
-                            font : {
-                                fontFamily : 'Tahoma',
-                                fontSize : 15
-                            }
-                        });
-                        row.add(dateTitleLbl);
+						dateTitleLbl = Ti.UI.createLabel({
+							text : 'التاريخ : ',
+							left : 0,
+							right : 10,
+							top : 38,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+							color : '#ffffff',
+							font : {
+								fontFamily : 'Tahoma',
+								fontSize : 15
+							}
+						});
+						row.add(dateTitleLbl);
 
-                        dateLbl = Ti.UI.createLabel({
-                            text : rows[i].date,
-                            left : 0,
-                            right : 60,
-                            top : 38,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-                            color : '#ffffff',
-                            font : {
-                                fontFamily : 'Tahoma',
-                                fontSize : 15
-                            }
-                        });
-                        row.add(dateLbl);
+						dateLbl = Ti.UI.createLabel({
+							text : rows[i].date,
+							left : 0,
+							right : 60,
+							top : 38,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+							color : '#ffffff',
+							font : {
+								fontFamily : 'Tahoma',
+								fontSize : 15
+							}
+						});
+						row.add(dateLbl);
 
-                        statusTitleLbl = Ti.UI.createLabel({
-                            text : 'حالة الطلب : ',
-                            right : 10,
-                            top : 63,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-                            color : '#ffffff',
-                            font : {
-                                fontFamily : 'Tahoma',
-                                fontSize : 15
-                            }
-                        });
-                        row.add(statusTitleLbl);
+						statusTitleLbl = Ti.UI.createLabel({
+							text : 'حالة الطلب : ',
+							right : 10,
+							top : 63,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+							color : '#ffffff',
+							font : {
+								fontFamily : 'Tahoma',
+								fontSize : 15
+							}
+						});
+						row.add(statusTitleLbl);
 
-                        statustxt = '';
-                        if (rows[i].status === '0') {
-                            statustxt = 'لم يتم البدأ';
-                        } else if (rows[i].status === '1') {
-                            statustxt = 'جاري الشحن';
-                        } else if (rows[i].status === '2') {
-                            statustxt = 'تم الوصول';
-                        }
+						statustxt = '';
+						if (rows[i].status === '0') {
+							statustxt = 'لم يتم البدأ';
+						} else if (rows[i].status === '1') {
+							statustxt = 'جاري الشحن';
+						} else if (rows[i].status === '2') {
+							statustxt = 'تم الوصول';
+						}
 
-                        if (rows[i].url) {
-                            statustxt += ' (اضغط لمتابعة الشحن)';
-                        }
+						if (rows[i].url) {
+							statustxt += ' (اضغط لمتابعة الشحن)';
+						}
 
-                        statusLbl = Ti.UI.createLabel({
-                            text : statustxt,
-                            right : 80,
-                            top : 63,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-                            color : '#ffffff',
-                            font : {
-                                fontFamily : 'Tahoma',
-                                fontSize : 15
-                            }
-                        });
-                        row.add(statusLbl);
+						statusLbl = Ti.UI.createLabel({
+							text : statustxt,
+							right : 80,
+							top : 63,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+							color : '#ffffff',
+							font : {
+								fontFamily : 'Tahoma',
+								fontSize : 15
+							}
+						});
+						row.add(statusLbl);
 
-                        tableRows.push(row);
-                    }
-                }
+						tableRows.push(row);
+					}
+				}
 
-                tableView.fireEvent('reloadData', {
-                    rows : tableRows
-                });
-            };
+				tableView.fireEvent('reloadData', {
+					rows : tableRows
+				});
+			};
 
-            xhr.send();
-        }
+			xhr.send();
+		}
 
-        tableView = Ti.UI.createTableView({
-            filterAttribute : 'myTitle',
-            backgroundColor : 'transparent',
-            separatorColor : 'transparent',
-            bottom : Ti.Platform.getOsname() == 'android' && 44
-        });
+		tableView = Ti.UI.createTableView({
+			filterAttribute : 'myTitle',
+			backgroundColor : 'transparent',
+			separatorColor : 'transparent',
+			bottom : Ti.Platform.getOsname() == 'android' && 44
+		});
 
-        tableView.addEventListener('runLoading', function() {
-            this.setData([{
-                title : 'جاري التحميل ....',
-                color : '#ffffff'
-            }]);
-        });
-        tableView.addEventListener('reloadData', function(e) {
-            this.setData(e.rows.length > 0 ? e.rows : [{
-                title : 'لا يوجد طلبات في الوقت الحالي !!',
-                color : '#ffffff'
-            }]);
-        });
+		tableView.addEventListener('runLoading', function() {
+			this.setData([{
+				title : 'جاري التحميل ....',
+				color : '#ffffff'
+			}]);
+		});
+		tableView.addEventListener('reloadData', function(e) {
+			this.setData(e.rows.length > 0 ? e.rows : [{
+				title : 'لا يوجد طلبات في الوقت الحالي !!',
+				color : '#ffffff'
+			}]);
+		});
 
-        tableView.addEventListener('click', function(e) {
+		tableView.addEventListener('click', function(e) {
 
-            if (e.rowData.data) {
+			if (e.rowData.data) {
 
-                Ti.App.fireEvent('openOrderProductsWindow', {
-                    data : e.rowData.data
-                });
-            }
-        });
+				Ti.App.fireEvent('openOrderProductsWindow', {
+					data : e.rowData.data
+				});
+			}
+		});
 
-        self.add(tableView);
+		self.add(tableView);
 
-        filterData();
+		filterData();
 
-        submitBtn = Ti.UI.createButton({
-            title : 'تحديث'
-        });
+		submitBtn = Ti.UI.createButton({
+			title : 'تحديث'
+		});
 
-        submitBtn.addEventListener('click', function() {
-            filterData();
-        });
-        if (Ti.Platform.getOsname() === 'android') {
-            submitBtn.bottom = 4;
-            submitBtn.height = 33;
-            submitBtn.width = '90%';
-            submitBtn.color = '#ffffff';
-            submitBtn.backgroundImage = '/images/button_ok.png';
+		submitBtn.addEventListener('click', function() {
+			filterData();
+		});
+		if (Ti.Platform.getOsname() === 'android') {
+			submitBtn.bottom = 4;
+			submitBtn.height = 33;
+			submitBtn.width = '90%';
+			submitBtn.color = '#ffffff';
+			submitBtn.backgroundImage = '/images/button_ok.png';
 
-            self.add(submitBtn);
-        } else {
-            self.setRightNavButton(submitBtn);
-        }
-        //setInterval(function(){filterData()},20000);
+			self.add(submitBtn);
+		} else {
+			self.setRightNavButton(submitBtn);
+		}
+		//setInterval(function(){filterData()},20000);
 
-        self.remove(loginBtn);
-        self.remove(msgLabel);
-    });
+		if (loginBtn) {
+			self.remove(loginBtn);
+			self.remove(msgLabel);
+		}
+	});
 
-    Ti.App.addEventListener('showMyordersBeforLogin', function() {
+	Ti.App.addEventListener('showMyordersBeforLogin', function() {
 
-        msgLabel = Ti.UI.createLabel({
-            text : 'يرجى تسجيل الدخول',
-            width : '100%',
-            top : 40,
-            color : '#ffffff',
-            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-            font : {
-                fontFamily : 'Arial',
-                fontSize : 17,
-                fontWeight : 'bold'
-            }
-        });
-        self.add(msgLabel);
+		msgLabel = Ti.UI.createLabel({
+			text : 'يرجى تسجيل الدخول',
+			width : '100%',
+			top : 40,
+			color : '#ffffff',
+			textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+			font : {
+				fontFamily : 'Arial',
+				fontSize : 17,
+				fontWeight : 'bold'
+			}
+		});
+		self.add(msgLabel);
 
-        loginBtn = auth.loginBtn(Ti.UI.createButton({
-            title : 'تسجيل دخول',
-            top : 40,
-            height : 33,
-            width : '90%',
-            left : '5%',
-            backgroundImage : '/images/button_ok.png',
-            color : '#ffffff'
-        }));
+		loginBtn = auth.loginBtn(Ti.UI.createButton({
+			title : 'تسجيل دخول',
+			top : 40,
+			height : 33,
+			width : '90%',
+			left : '5%',
+			backgroundImage : '/images/button_ok.png',
+			color : '#ffffff'
+		}));
 
-        self.add(loginBtn);
+		self.add(loginBtn);
 
-        if (tableView) {
-            self.remove(tableView);
-            self.remove(dateTitleLbl);
-            self.remove(dateLbl);
-            self.remove(totaltitleLbl);
-            self.remove(priceLbl);
-            self.remove(statusTitleLbl);
-            self.remove(statusLbl);
-            self.remove(submitBtn);
-        }
-    });
+		if (tableView) {
+			self.remove(tableView);
+		}
+		if (dateTitleLbl) {
+			self.remove(dateTitleLbl);
+			self.remove(dateLbl);
+			self.remove(totaltitleLbl);
+			self.remove(priceLbl);
+			self.remove(statusTitleLbl);
+			self.remove(statusLbl);
+			self.remove(submitBtn);
+		}
+	});
 
-    if (!auth.isLogedIn()) {
+	if (!auth.isLogedIn()) {
 
-        Ti.App.fireEvent('showMyordersBeforLogin');
-    } else {
+		Ti.App.fireEvent('showMyordersBeforLogin');
+	} else {
 
-        Ti.App.fireEvent('showMyordersAfterLogin');
-    }
-    return self;
+		Ti.App.fireEvent('showMyordersAfterLogin');
+	}
+	return self;
 }
 
 module.exports = myOrdersWin;
--- Resources/ui/common/walletWin.js
+++ Resources/ui/common/walletWin.js
@@ -1,182 +1,184 @@
 function walletWin() {
 
-    var self, loadinLabel, chargeBtn, balanceLbl, balancCurr, img, loginBtn, msgLabel, auth = require('/lib/auth'), balanceTitle;
+	var self, loadinLabel, chargeBtn, balanceLbl, balancCurr, img, loginBtn, msgLabel, auth = require('/lib/auth'), balanceTitle;
 
-    self = Ti.UI.createWindow({
-        title : 'المحفظة',
-        backgroundImage : '/images/bg.jpg',
-        barImage : '/images/Navigation_Bar.jpg',
-        barColor : 'gray'
-    });
+	self = Ti.UI.createWindow({
+		title : 'المحفظة',
+		backgroundImage : '/images/bg.jpg',
+		barImage : '/images/Navigation_Bar.jpg',
+		barColor : 'gray'
+	});
 
-    Ti.App.addEventListener('showWalletAfterLogin', function() {
+	Ti.App.addEventListener('showWalletAfterLogin', function() {
 
-        loadinLabel = Ti.UI.createLabel({
-            text : 'جاري التحميل ....',
-            //left : 0,
-            //right : 10,
-            //top : 10,
-            color : '#000000',
-            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER
-        });
-        self.add(loadinLabel);
+		loadinLabel = Ti.UI.createLabel({
+			text : 'جاري التحميل ....',
+			//left : 0,
+			//right : 10,
+			//top : 10,
+			color : '#000000',
+			textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER
+		});
+		self.add(loadinLabel);
 
-        function getBalance() {
+		function getBalance() {
 
-            var xhr = Ti.Network.createHTTPClient();
+			var xhr = Ti.Network.createHTTPClient();
 
-            xhr.open('GET', Ti.App.APIURL + 'api/walletBalance/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency', 1));
+			xhr.open('GET', Ti.App.APIURL + 'api/walletBalance/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency', 1));
 
-            xhr.onerror = function() {
+			xhr.onerror = function() {
 
-                loadinLabel.setText('لا يوجد نتائج هنا في الوقت الحالي !!');
-            };
+				loadinLabel.setText('لا يوجد نتائج هنا في الوقت الحالي !!');
+			};
 
-            xhr.onload = function() {
-                try {
-                    var row = JSON.parse(this.responseText);
-                } catch(e) {
-                    Ti.UI.createAlertDialog({
-                        message : 'خطأ في الآتصال، تاكد من اتصال الانترنت الخاص بك.',
-                        cancel : 0,
-                        buttonNames : ['موافق']
-                    }).show();
+			xhr.onload = function() {
+				try {
+					var row = JSON.parse(this.responseText);
+				} catch(e) {
+					Ti.UI.createAlertDialog({
+						message : 'خطأ في الآتصال، تاكد من اتصال الانترنت الخاص بك.',
+						cancel : 0,
+						buttonNames : ['موافق']
+					}).show();
 
-                    return false;
-                }
-                balanceLbl.setText(row.balance);
-                loadinLabel.setVisible(false);
-                balanceTitle.setVisible(true);
-                balanceLbl.setVisible(true);
-                balancCurr.setVisible(true);
-            };
+					return false;
+				}
+				balanceLbl.setText(row.balance);
+				loadinLabel.setVisible(false);
+				balanceTitle.setVisible(true);
+				balanceLbl.setVisible(true);
+				balancCurr.setVisible(true);
+			};
 
-            xhr.send();
-        }
+			xhr.send();
+		}
 
-        chargeBtn = Ti.UI.createButton({
-            title : 'شحن المحفظة',
-            top : 200,
-            height : 33,
-            width : '90%',
-            color : '#ffffff',
-            backgroundImage : '/images/button_ok.png'
-        });
+		chargeBtn = Ti.UI.createButton({
+			title : 'شحن المحفظة',
+			top : 200,
+			height : 33,
+			width : '90%',
+			color : '#ffffff',
+			backgroundImage : '/images/button_ok.png'
+		});
 
-        chargeBtn.addEventListener('click', function() {
-            Ti.App.fireEvent('openChargeWalletWindow');
-        });
+		chargeBtn.addEventListener('click', function() {
+			Ti.App.fireEvent('openChargeWalletWindow');
+		});
 
-        self.add(chargeBtn);
+		self.add(chargeBtn);
 
-        img = Ti.UI.createImageView({
-            image : '/images/wallet_balance.png',
-            width : 169,
-            height : 148,
-            top : '40px'
-        });
-        self.add(img);
+		img = Ti.UI.createImageView({
+			image : '/images/wallet_balance.png',
+			width : 169,
+			height : 148,
+			top : '40px'
+		});
+		self.add(img);
 
-        balanceTitle = Ti.UI.createLabel({
-            text : 'الرصيد',
-            //left : 0,
-            //right : 150,
-            top : 60,
-            color : '#000000',
-            visible : false,
-            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-            font : {
-                fontFamily : 'Arial',
-                fontSize : 16,
-                fontWeight : 'bold'
-            }
-        });
-        self.add(balanceTitle);
+		balanceTitle = Ti.UI.createLabel({
+			text : 'الرصيد',
+			//left : 0,
+			//right : 150,
+			top : 60,
+			color : '#000000',
+			visible : false,
+			textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+			font : {
+				fontFamily : 'Arial',
+				fontSize : 16,
+				fontWeight : 'bold'
+			}
+		});
+		self.add(balanceTitle);
 
-        balanceLbl = Ti.UI.createLabel({
-            text : '',
-            //left : 0,
-            //right : 155,
-            top : 85,
-            visible : false,
-            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-            color : 'red',
-            font : {
-                fontFamily : 'Arial',
-                fontSize : 20,
-                fontWeight : 'bold'
-            }
-        });
-        Ti.App.balanceLbl = balanceLbl;
-        self.add(Ti.App.balanceLbl);
+		balanceLbl = Ti.UI.createLabel({
+			text : '',
+			//left : 0,
+			//right : 155,
+			top : 85,
+			visible : false,
+			textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+			color : 'red',
+			font : {
+				fontFamily : 'Arial',
+				fontSize : 20,
+				fontWeight : 'bold'
+			}
+		});
+		Ti.App.balanceLbl = balanceLbl;
+		self.add(Ti.App.balanceLbl);
 
-        balancCurr = Ti.UI.createLabel({
-            text : Ti.App.Properties.getString('currencyName', 'دولار أمريكي'),
-            //left : 0,
-            //right : 127,
-            top : 110,
-            color : '#000000',
-            visible : false,
-            textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-            font : {
-                fontFamily : 'Arial',
-                fontSize : 15,
-                fontWeight : 'bold'
-            }
-        });
-        self.add(balancCurr);
+		balancCurr = Ti.UI.createLabel({
+			text : Ti.App.Properties.getString('currencyName', 'دولار أمريكي'),
+			//left : 0,
+			//right : 127,
+			top : 110,
+			color : '#000000',
+			visible : false,
+			textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+			font : {
+				fontFamily : 'Arial',
+				fontSize : 15,
+				fontWeight : 'bold'
+			}
+		});
+		self.add(balancCurr);
 
-        getBalance();
+		getBalance();
 
-        self.remove(loginBtn);
-        self.remove(msgLabel);
-    });
+		if (loginBtn) {
+			self.remove(loginBtn);
+			self.remove(msgLabel);
+		}
+	});
 
-    Ti.App.addEventListener('showWalletBeforLogin', function() {
+	Ti.App.addEventListener('showWalletBeforLogin', function() {
 
-        msgLabel = Ti.UI.createLabel({
-            text : 'يرجى تسجيل الدخول',
-            width : '100%',
-            top : 40,
-            color : '#ffffff',
-            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-            font : {
-                fontFamily : 'Arial',
-                fontSize : 17,
-                fontWeight : 'bold'
-            }
-        });
-        self.add(msgLabel);
+		msgLabel = Ti.UI.createLabel({
+			text : 'يرجى تسجيل الدخول',
+			width : '100%',
+			top : 40,
+			color : '#ffffff',
+			textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+			font : {
+				fontFamily : 'Arial',
+				fontSize : 17,
+				fontWeight : 'bold'
+			}
+		});
+		self.add(msgLabel);
 
-        loginBtn = auth.loginBtn(Ti.UI.createButton({
-            title : 'تسجيل دخول',
-            top : 100,
-            height : 33,
-            width : '90%',
-            backgroundImage : '/images/button_ok.png',
-            color : '#ffffff'
-        }));
-        self.add(loginBtn);
+		loginBtn = auth.loginBtn(Ti.UI.createButton({
+			title : 'تسجيل دخول',
+			top : 100,
+			height : 33,
+			width : '90%',
+			backgroundImage : '/images/button_ok.png',
+			color : '#ffffff'
+		}));
+		self.add(loginBtn);
 
-        if (balancCurr) {
-            self.remove(balancCurr);
-            self.remove(Ti.App.balanceLbl);
-            self.remove(balanceTitle);
-            self.remove(img);
-            self.remove(loadinLabel);
-            self.remove(chargeBtn);
-        }
-    });
+		if (balancCurr) {
+			self.remove(balancCurr);
+			self.remove(Ti.App.balanceLbl);
+			self.remove(balanceTitle);
+			self.remove(img);
+			self.remove(loadinLabel);
+			self.remove(chargeBtn);
+		}
+	});
 
-    if (!auth.isLogedIn()) {
+	if (!auth.isLogedIn()) {
 
-        Ti.App.fireEvent('showWalletBeforLogin');
-    } else {
+		Ti.App.fireEvent('showWalletBeforLogin');
+	} else {
 
-        Ti.App.fireEvent('showWalletAfterLogin');
-    }
+		Ti.App.fireEvent('showWalletAfterLogin');
+	}
 
-    return self;
+	return self;
 }
 
 module.exports = walletWin;
