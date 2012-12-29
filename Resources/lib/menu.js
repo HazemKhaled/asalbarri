64a5fc1f1c74cf2e5fcf170762537ee978d7717c
Hazem Khaled <hazem.khaled@gmail.com>
2012-12-30 01:33:01 +0200

    updates in menu after login

--- Resources/lib/menu.js
+++ Resources/lib/menu.js
@@ -1,132 +1,126 @@
 function menusGenerator(self) {
-    var aboutBtn, settingBtn, optionsDialogOpts, dialog, auth, linksArray = [
-            Ti.App.Properties.getString('currencyName', 'دولار أمريكي') + ' (تغيير)',
-            'اخبارنا',
-            'س و ج',
-            'عروض خاصة',
-            'رسائل SMS'
-        ];
+	var aboutBtn, settingBtn, optionsDialogOpts, dialog, auth, linksArray = [Ti.App.Properties.getString('currencyName', 'دولار أمريكي') + ' (تغيير)', 'اخبارنا', 'س و ج', 'عروض خاصة', 'رسائل SMS'];
 
-    //openAboutWindow
-    if (Ti.Platform.getOsname() === 'android') {
-        self.activity.onCreateOptionsMenu = function(e) {
-            aboutBtn = e.menu.add({
-                title : 'اعدادات',
-                icon : '/images/action_settings.png',
-                showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM
-            });
+	//openAboutWindow
+	if (Ti.Platform.getOsname() === 'android') {
+		self.activity.onCreateOptionsMenu = function(e) {
+			aboutBtn = e.menu.add({
+				title : 'اعدادات',
+				icon : '/images/action_settings.png',
+				showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM
+			});
 
-            aboutBtn.addEventListener('click', function() {
-                dialog.show();
-            });
+			aboutBtn.addEventListener('click', function() {
+				dialog.show();
+			});
 
-            aboutBtn = e.menu.add({
-                title : 'عن البرنامج',
-                icon : '/images/action_about.png',
-                showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM
-            });
+			aboutBtn = e.menu.add({
+				title : 'عن البرنامج',
+				icon : '/images/action_about.png',
+				showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM
+			});
 
-            aboutBtn.addEventListener('click', function() {
-                Ti.App.fireEvent('openAboutWindow');
-            });
-        };
-    } else {
-        if (self.hideAboutBtn == false) {// only on home window
-            aboutBtn = Ti.UI.createButton({
-                height : 31,
-                width : 31,
-                color : '#000000',
-                backgroundImage : '/images/icon_2.png'
-            });
+			aboutBtn.addEventListener('click', function() {
+				Ti.App.fireEvent('openAboutWindow');
+			});
+		};
+	} else {
+		if (self.hideAboutBtn == false) {// only on home window
+			aboutBtn = Ti.UI.createButton({
+				height : 31,
+				width : 31,
+				color : '#000000',
+				backgroundImage : '/images/icon_2.png'
+			});
 
-            aboutBtn.addEventListener('click', function() {
-                Ti.App.fireEvent('openAboutWindow');
-            });
-            self.setLeftNavButton(aboutBtn);
-        }
-        //openSettingWindow
-        settingBtn = Ti.UI.createButton({
-            height : 31,
-            width : 31,
-            color : '#000000',
-            backgroundImage : '/images/icon_1.png'
-        });
-        settingBtn.addEventListener('click', function() {
-            dialog.show();
-        });
+			aboutBtn.addEventListener('click', function() {
+				Ti.App.fireEvent('openAboutWindow');
+			});
+			self.setLeftNavButton(aboutBtn);
+		}
+		//openSettingWindow
+		settingBtn = Ti.UI.createButton({
+			height : 31,
+			width : 31,
+			color : '#000000',
+			backgroundImage : '/images/icon_1.png'
+		});
+		settingBtn.addEventListener('click', function() {
+			dialog.show();
+		});
 
-        self.setRightNavButton(settingBtn);
-    }
+		self.setRightNavButton(settingBtn);
+	}
 
-    // options dialog
-    optionsDialogOpts = {
-        options : linksArray.concat(['تسجيل دخول', 'تسجيل جديد', 'اغلاق']),
-        cancel : 7,
-        title : 'اعدادات'
-    };
+	// options dialog
+	optionsDialogOpts = {
+		options : linksArray.concat(['تسجيل دخول', 'تسجيل جديد', 'اغلاق']),
+		cancel : 7,
+		title : 'اعدادات'
+	};
 
-    dialog = Ti.UI.createOptionDialog(optionsDialogOpts);
+	dialog = Ti.UI.createOptionDialog(optionsDialogOpts);
 
-    auth = require('/lib/auth');
-    if (auth.isLogedIn() !== false) {
-        dialog.options = linksArray.concat(['تسجيل خروج', 'بيانات المستخدم', 'اغلاق']);
-        dialog.destructive = 4;
-    }
+	auth = require('/lib/auth');
+	if (auth.isLogedIn() !== false) {
+		dialog.options = linksArray.concat(['تغير بيناتي', 'تسجيل خروج', 'اغلاق']);
+		dialog.destructive = 6;
+	}
 
-    // add event listener
-    dialog.addEventListener('click', function(e) {
-        //aboutBtn.title = 'You selected ' + e.index;
-        if (auth.isLogedIn() === false) {
-            switch(e.index) {
-                case 5:
+	// add event listener
+	dialog.addEventListener('click', function(e) {
+		//aboutBtn.title = 'You selected ' + e.index;
+		if (auth.isLogedIn() === false) {
+			switch(e.index) {
+				case 5:
 
-                    Ti.App.fireEvent('openLoginWindow');
-                    break;
-                case 6:
+					Ti.App.fireEvent('openLoginWindow');
+					break;
+				case 6:
 
-                    Ti.App.fireEvent('openRegisterWindow');
-                    break;
-            }
-        } else {
-            switch(e.index) {
-                case 5:
+					Ti.App.fireEvent('openRegisterWindow');
+					break;
+			}
+		} else {
+			switch(e.index) {
+				case 5:
 
-                    Ti.App.Properties.removeProperty('userID');
-                    Ti.App.fireEvent('cartEmpty');
-                    Ti.App.fireEvent('showWalletBeforLogin');
-                    Ti.App.fireEvent('showMyordersBeforLogin');
-                    Ti.App.fireEvent('closeOrderProductsWindow');
-                    dialog.options = linksArray.concat(['تسجيل دخول', 'تسجيل جديد', 'اغلاق']);
-                    break;
-                case 6:
+					var userNameMsg = Ti.UI.createAlertDialog({
+						title : 'بيانات المستخدم',
+						message : 'العضو الحالي هو : ' + Ti.App.Properties.getString('userName')
+					});
+					userNameMsg.show();
+					break;
+				case 6:
 
-                    var userNameMsg = Ti.UI.createAlertDialog({
-                        title : 'بيانات المستخدم',
-                        message : 'العضو الحالي هو : ' + Ti.App.Properties.getString('userName')
-                    });
-                    userNameMsg.show();
-                    break;
-            }
-        }
+					Ti.App.Properties.removeProperty('userID');
+					Ti.App.fireEvent('cartEmpty');
+					Ti.App.fireEvent('showWalletBeforLogin');
+					Ti.App.fireEvent('showMyordersBeforLogin');
+					Ti.App.fireEvent('closeOrderProductsWindow');
+					dialog.options = linksArray.concat(['تسجيل دخول', 'تسجيل جديد', 'اغلاق']);
+					break;
+			}
+		}
 
-        switch(e.index) {
-            case 0:
-                Ti.App.fireEvent('openCurrencyWindow');
-                break;
-            case 1:
-                Ti.App.fireEvent('openNewsWindow');
-                break;
-            case 2:
-                Ti.App.fireEvent('openFaqWindow');
-                break;
-            case 3:
-                var OffersListWinModule = require('ui/common/offersListWin');
-                new OffersListWinModule().open();
-                break;
-            case 4:
-                var OffersListWinModule = require('ui/common/SMSProWin');
-                new OffersListWinModule().open();
-                break;
-        }
-    });
+		switch(e.index) {
+			case 0:
+				Ti.App.fireEvent('openCurrencyWindow');
+				break;
+			case 1:
+				Ti.App.fireEvent('openNewsWindow');
+				break;
+			case 2:
+				Ti.App.fireEvent('openFaqWindow');
+				break;
+			case 3:
+				var OffersListWinModule = require('ui/common/offersListWin');
+				new OffersListWinModule().open();
+				break;
+			case 4:
+				var OffersListWinModule = require('ui/common/SMSProWin');
+				new OffersListWinModule().open();
+				break;
+		}
+	});
 }
