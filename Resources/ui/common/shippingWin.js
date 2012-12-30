e028b87eb880bf0a4fc78eb6a39729e29d2b255a
Hazem Khaled <hazem.khaled@gmail.com>
2012-12-30 02:56:48 +0200

    Accept terms window on order Remove select country from order window

--- Resources/ui/common/shippingWin.js
+++ Resources/ui/common/shippingWin.js
@@ -1,135 +1,123 @@
 function shippingWin() {
 
-    var self, tableView, xhr, countriesPicker, nextBtn;
+	var self, tableView, xhr, nextBtn;
 
-    self = Ti.UI.createWindow({
-        title : 'اختر الدولة',
-        backgroundImage : '/images/bg.jpg',
-        barImage : '/images/Navigation_Bar.jpg',
-        barColor : 'gray'
-    });
+	self = Ti.UI.createWindow({
+		title : 'اتفاقية الاستخدام',
+		backgroundImage : '/images/bg.jpg',
+		barImage : '/images/Navigation_Bar.jpg',
+		barColor : 'gray'
+	});
 
-    nextBtn = Ti.UI.createButton({
-        title : '< التالي'
-    });
+	nextBtn = Ti.UI.createButton({
+		title : '< موافق'
+	});
 
-    nextBtn.addEventListener('click', function() {
+	nextBtn.addEventListener('click', function() {
 
-        var walletFlag = false;
+		var walletFlag = false;
 
-        // if total = 0
-        if (Ti.App.cartQuantityCounter().totalNet <= 0) {
-            Ti.App.fireEvent('orderRequest', {
-                paymentMethod : 'wallet',
-                countryID : countriesPicker.getSelectedRow(0).myId
-            });
-            return;
-        }
+		// if total = 0
+		if (Ti.App.cartQuantityCounter().totalNet <= 0) {
+			Ti.App.fireEvent('orderRequest', {
+				paymentMethod : 'wallet',
+				//countryID : countriesPicker.getSelectedRow(0).myId
+			});
+			return;
+		}
 
-        //Ti.App.Properties.setInt('country', countriesPicker.getSelectedRow(0).myId);
+		Ti.App.getHttpRequest('api/walletBalance/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency', 1), function(results) {
 
-        Ti.App.getHttpRequest('api/walletBalance/' + Ti.App.Properties.getInt('userID') + '/' + Ti.App.Properties.getInt('currency', 1), function(results) {
+			var paymentAlert = Ti.UI.createAlertDialog({
+				title : 'اختر وسيلة الدفع',
+				message : 'اي هذه الوسائل تريد ان تدفع من خلالها؟',
+				cancel : 2,
+				buttonNames : ['الدفع بالفيزا عبر 2CO', 'المحفظة ' + results.balance + ' ' + Ti.App.Properties.getString('currencyName', 'دولار أمريكي'), 'عودة لسلة التسوق']
+			});
 
-            var paymentAlert = Ti.UI.createAlertDialog({
-                title : 'اختر وسيلة الدفع',
-                message : 'اي هذه الوسائل تريد ان تدفع من خلالها؟',
-                cancel : 2,
-                buttonNames : ['الدفع بالفيزا عبر 2CO', 'المحفظة ' + results.balance + ' ' + Ti.App.Properties.getString('currencyName', 'دولار أمريكي'), 'عودة لسلة التسوق']
-            });
+			paymentAlert.show();
 
-            paymentAlert.show();
+			paymentAlert.addEventListener('click', function(e) {
 
-            paymentAlert.addEventListener('click', function(e) {
+				switch (e.index) {
+					case 0:
 
-                switch (e.index) {
-                    case 0:
+						Ti.App.fireEvent('orderRequest', {
+							paymentMethod : 'tocheckout',
+							//countryID : countriesPicker.getSelectedRow(0).myId
+						});
+						break;
+					case 1:
 
-                        Ti.App.fireEvent('orderRequest', {
-                            paymentMethod : 'tocheckout',
-                            countryID : countriesPicker.getSelectedRow(0).myId
-                        });
-                        break;
-                    case 1:
+						// is it enoph cash into wallet?
+						if (results.balance < Ti.App.cartQuantityCounter().totalNet) {
 
-                        // is it enoph cash into wallet?
-                        if (results.balance < Ti.App.cartQuantityCounter().totalNet) {
+							// if wallet not enph and he want to recharge his cridit
+							if (walletFlag === true) {
+								if (Ti.Platform.getOsname() !== 'android') {
+									Ti.App.walletTab.setActive(true);
+								} else {
+									Ti.UI.createAlertDialog({
+										title : 'اضغط على المحفظة من الاعلى',
+										message : 'عد للخلف ثم اضغط على المحفظة من الاعلى.',
+										cancel : 0,
+										buttonNames : ['موافق']
+									}).show();
+								}
 
-                            // if wallet not enph and he want to recharge his cridit
-                            if (walletFlag === true) {
-                                if (Ti.Platform.getOsname() !== 'android') {
-                                    Ti.App.walletTab.setActive(true);
-                                } else {
-                                    Ti.UI.createAlertDialog({
-                                        title : 'اضغط على المحفظة من الاعلى',
-                                        message : 'عد للخلف ثم اضغط على المحفظة من الاعلى.',
-                                        cancel : 0,
-                                        buttonNames : ['موافق']
-                                    }).show();
-                                }
+								return;
+							}
 
-                                return;
-                            }
+							// next time open wallet directly
+							walletFlag = true;
 
-                            // next time open wallet directly
-                            walletFlag = true;
+							// change alert msgs
+							paymentAlert.buttonNames = ['الدفع بالفيزا عبر 2CO', 'شحن المحفظة', 'عودة لسلة التسوق'];
+							paymentAlert.setTitle('الرصيد لا يكفي !');
+							paymentAlert.setMessage('رصيد المحفظة لا يكفي، يمكنك شحن المحفظة او اختيار وسيلة دفع اخرى.');
 
-                            // change alert msgs
-                            paymentAlert.buttonNames = ['الدفع بالفيزا عبر 2CO', 'شحن المحفظة', 'عودة لسلة التسوق'];
-                            paymentAlert.setTitle('الرصيد لا يكفي !');
-                            paymentAlert.setMessage('رصيد المحفظة لا يكفي، يمكنك شحن المحفظة او اختيار وسيلة دفع اخرى.');
+							paymentAlert.show();
+							break;
+						}
 
-                            paymentAlert.show();
-                            break;
-                        }
+						Ti.App.fireEvent('orderRequest', {
+							paymentMethod : 'wallet',
+							//countryID : countriesPicker.getSelectedRow(0).myId
+						});
 
-                        Ti.App.fireEvent('orderRequest', {
-                            paymentMethod : 'wallet',
-                            countryID : countriesPicker.getSelectedRow(0).myId
-                        });
+						break;
+					case 2:
 
-                        break;
-                    case 2:
+						self.close();
+						break;
+				}
 
-                        self.close();
-                        break;
-                }
+			});
+		});
+	});
 
-            });
-        });
-    });
+	if (Ti.Platform.getOsname() === 'android') {
 
-    if (Ti.Platform.getOsname() === 'android') {
+		nextBtn.height = 33;
+		nextBtn.width = '90%';
+		nextBtn.bottom = 4;
+		nextBtn.backgroundImage = '/images/button_ok.png';
+		nextBtn.color = '#ffffff';
 
-        nextBtn.height = 33;
-        nextBtn.width = '90%';
-        nextBtn.bottom = 4;
-        nextBtn.backgroundImage = '/images/button_ok.png';
-        nextBtn.color = '#ffffff';
+		self.add(nextBtn);
+	} else {
+		self.setRightNavButton(nextBtn);
+	}
 
-        self.add(nextBtn);
-    } else {
-        self.setRightNavButton(nextBtn);
-    }
+	Ti.App.getHttpRequest('api/agreement', function(html) {
+		self.add(Ti.UI.createWebView({
+			html : '<html><body style="direction: rtl; background: url(/images/bg.jpg) fixed; color: #fff">' + html + '</body></html>'
+		}));
+	}, function() {
+		self.close();
+	});
 
-    Ti.App.getHttpRequest('api/getCountries', function(rows) {
-        var index, pickerRows;
-
-        countriesPicker = Ti.UI.createPicker({
-            selectionIndicator : true
-        });
-
-        for (index in rows) {
-            countriesPicker.add(Ti.UI.createPickerRow({
-                title : rows[index].title,
-                myId : rows[index].id
-            }));
-        }
-        self.add(countriesPicker);
-    }, function() {
-        self.close();
-    });
-
-    return self;
+	return self;
 }
 
 module.exports = shippingWin;
