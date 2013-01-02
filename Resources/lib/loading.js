6a289ec460d45dd8558784dcea7368024b21e2da
Hazem Khaled <hazem.khaled@gmail.com>
2013-01-03 01:09:38 +0200

    fix in loading

--- Resources/lib/loading.js
+++ Resources/lib/loading.js
@@ -1,14 +1,13 @@
 var indWin = Ti.UI.createWindow({
 	backgroundColor : '#111111',
-	opacity : 0.6,
-	fullscreen : true
+	opacity : 0.7
 });
 // black view
 var indView = Ti.UI.createView({
 	layout : 'vertical',
 	backgroundColor : '#000000',
 	borderRadius : 10,
-	opacity : 0.9,
+	//opacity : 0.9,
 	height : 100,
 	width : 150
 });
@@ -25,8 +24,6 @@ var activityIndicator = Ti.UI.createActivityIndicator({
 	height : Ti.UI.SIZE,
 	width : Ti.UI.SIZE
 });
-
-// The activity indicator must be added to a window or view for it to appear
 indView.add(activityIndicator);
 
 var messageLbl = Ti.UI.createLabel({
@@ -38,18 +35,17 @@ var messageLbl = Ti.UI.createLabel({
 		fontSize : 20,
 		fontWeight : 'bold'
 	},
-	top : 10
+	top : 10,
+	bottom : 10
 });
 indView.add(messageLbl);
 
 exports.hide = function() {
-
 	indWin.close();
 	activityIndicator.hide();
 };
 
 exports.show = function() {
-
 	indWin.open();
 	activityIndicator.show();
 };
@@ -60,9 +56,5 @@ exports.setMsg = function(msg, loadingTxt) {
 		loadingTxt = 'جاري التحميل';
 	}
 
-	if (Ti.Platform.getOsname() != 'android') {
-		messageLbl.text = loadingTxt + "\n" + msg;
-	} else {
-		activityIndicator.message = loadingTxt + "\n" + msg;
-	}
+	messageLbl.text = loadingTxt + "\n" + msg;
 };
