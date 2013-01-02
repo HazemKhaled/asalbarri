2f8814c064a996ffdb9cc7e010e0a709d439860b
Hazem Khaled <hazem.khaled@gmail.com>
2013-01-03 00:55:51 +0200

    change loading with sdk 3.0

--- Resources/lib/loading.js
+++ Resources/lib/loading.js
@@ -1,102 +1,68 @@
-/**
- *
- * @Auther Hazem Khaled <Hazem.Khald@gmail.com>
- *
- Ti.App.addEventListener('showLoading', function(e) {
+var indWin = Ti.UI.createWindow({
+	backgroundColor : '#111111',
+	opacity : 0.6,
+	fullscreen : true
+});
+// black view
+var indView = Ti.UI.createView({
+	layout : 'vertical',
+	backgroundColor : '#000000',
+	borderRadius : 10,
+	opacity : 0.9,
+	height : 100,
+	width : 150
+});
+indWin.add(indView);
 
- var loading = require('lib/loading');
-
- loading.show();
-
- Ti.App.addEventListener('hideLoading', function(e) {
- loading.hide();
- });
-
- Ti.App.addEventListener('msgLoading', function(e) {
- loading.setMsg(e.text);
- });
- });
- */
-
-var indWin = null, actInd = null;
-
-var messageLbl = Ti.UI.createLabel({
-    text : 'جاري التحميل',
-    color : '#fff',
-    textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-    font : {
-        fontSize : 20,
-        fontWeight : 'bold'
-    },
-    top : 260
+if (Ti.Platform.name === 'iPhone OS') {
+	var style = Ti.UI.iPhone.ActivityIndicatorStyle.BIG;
+} else {
+	var style = Ti.UI.ActivityIndicatorStyle.BIG;
+}
+var activityIndicator = Ti.UI.createActivityIndicator({
+	style : style,
+	top : 10,
+	height : Ti.UI.SIZE,
+	width : Ti.UI.SIZE
 });
 
+// The activity indicator must be added to a window or view for it to appear
+indView.add(activityIndicator);
+
+var messageLbl = Ti.UI.createLabel({
+	text : 'جاري التحميل',
+	color : '#fff',
+	textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+	font : {
+		fontFamily : 'Helvetica Neue',
+		fontSize : 20,
+		fontWeight : 'bold'
+	},
+	top : 10
+});
+indView.add(messageLbl);
+
 exports.hide = function() {
-    if (Ti.Platform.getOsname() !== 'android') {
-        if (indWin !== null) {
-            indWin.close({
-                opacity : 0,
-                duration : 500
-            });
-        }
-        indWin = null;
-    } else {
-        if (actInd) {
-            actInd.hide();
-            actInd = null;
-        }
-    }
+
+	indWin.close();
+	activityIndicator.hide();
 };
 
 exports.show = function() {
 
-    if (Ti.Platform.getOsname() !== 'android') {
-        // window container
-        indWin = Ti.UI.createWindow({
-            //height : 150,
-            //width : 150,
-            backgroundColor : '#111111',
-            opacity : 0.6
-        });
-
-        // black view
-        var indView = Ti.UI.createView({
-            height : 170,
-            width : 150,
-            backgroundColor : '#000000',
-            borderRadius : 10,
-            opacity : 0.9
-        });
-        indWin.add(indView);
-    }
-
-    // loading indicator
-    actInd = Ti.UI.createActivityIndicator({
-        height : 30,
-        width : 30
-    });
-
-    if (Ti.Platform.getOsname() !== 'android') {
-        actInd.style = Ti.UI.iPhone.ActivityIndicatorStyle.BIG;
-        indWin.add(actInd);
-
-        indWin.add(messageLbl);
-        indWin.open();
-    } else {
-        actInd.message = "جاري التحميل";
-    }
-    actInd.show();
+	indWin.open();
+	activityIndicator.show();
 };
 
 exports.setMsg = function(msg, loadingTxt) {
 
-    if (loadingTxt == undefined) {
-        loadingTxt = 'جاري التحميل';
-    }
+	if (loadingTxt == undefined) {
+		loadingTxt = 'جاري التحميل';
+	}
 
-    if (Ti.Platform.getOsname() != 'android') {
-        messageLbl.text = loadingTxt + "\n" + msg;
-    } else {
-        actInd.message = loadingTxt + "\n" + msg;
-    }
+	if (Ti.Platform.getOsname() != 'android') {
+		messageLbl.text = loadingTxt + "\n" + msg;
+	} else {
+		activityIndicator.message = loadingTxt + "\n" + msg;
+	}
 };
