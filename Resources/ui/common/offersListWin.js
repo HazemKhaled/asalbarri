88bebbdb052e31dc42c49f24a0b3d4f9cc9a280b
Hazem Khaled <hazem.khaled@gmail.com>
2013-01-07 17:44:03 +0200

    effect change currency in offers

--- Resources/ui/common/offersListWin.js
+++ Resources/ui/common/offersListWin.js
@@ -1,243 +1,244 @@
 function offersListWin() {
 
-    var self, tableView;
-    self = Ti.UI.createWindow({
-        title : 'عروض خاصة',
-        hideAboutBtn : false,
-        backButtonTitle : 'عودة',
-        backgroundImage : '/images/bg.jpg',
-        barImage : '/images/Navigation_Bar.jpg',
-        barColor : 'gray'
-    });
+	var self, tableView;
+	self = Ti.UI.createWindow({
+		title : 'عروض خاصة',
+		hideAboutBtn : false,
+		backButtonTitle : 'عودة',
+		backgroundImage : '/images/bg.jpg',
+		barImage : '/images/Navigation_Bar.jpg',
+		barColor : 'gray'
+	});
 
-    Ti.include('/lib/menu.js');
-    menusGenerator(self);
+	Ti.include('/lib/menu.js');
+	menusGenerator(self);
 
-    function filterData() {
+	function filterData() {
 
-        tableView.fireEvent('runLoading');
+		tableView.fireEvent('runLoading');
 
-        var tableRows = [], xhr;
+		var tableRows = [], xhr;
 
-        xhr = Ti.Network.createHTTPClient();
+		xhr = Ti.Network.createHTTPClient();
 
-        xhr.open('GET', Ti.App.APIURL + 'api/productsByOffers/' + Ti.App.Properties.getInt('currency', 2));
+		xhr.open('GET', Ti.App.APIURL + 'api/productsByOffers/' + Ti.App.Properties.getInt('currency', 2));
 
-        xhr.onerror = function() {
-            tableView.fireEvent('reloadData', {
-                rows : []
-            });
-        };
+		xhr.onerror = function() {
+			tableView.fireEvent('reloadData', {
+				rows : []
+			});
+		};
 
-        xhr.onload = function() {
-            var rows, i, row, img, titleLbl, purchasesQtyLbl, priceFitLbl, priceLbl, arrow, pricebackground, priceLblCurr, priceFitLbl;
+		xhr.onload = function() {
+			var rows, i, row, img, titleLbl, purchasesQtyLbl, priceFitLbl, priceLbl, arrow, pricebackground, priceLblCurr, priceFitLbl;
 
-            try {
-                rows = JSON.parse(this.responseText);
-            } catch (e) {
+			try {
+				rows = JSON.parse(this.responseText);
+			} catch (e) {
 
-                tableView.fireEvent('reloadData', {
-                    rows : []
-                });
-                return false;
-            }
+				tableView.fireEvent('reloadData', {
+					rows : []
+				});
+				return false;
+			}
 
-            for (i in rows) {
-                if (rows.hasOwnProperty(i)) {
-                    row = Ti.UI.createTableViewRow({
-                        height : 95,
-                        myTitle : rows[i].title,
-                        data : rows[i],
-                        className : 'productRow',
-                        backgroundImage : '/images/TableViewRowBG.jpg',
-                        selectedBackgroundImage : '/images/TableViewRowSelectedBG.png'
-                    });
+			for (i in rows) {
+				if (rows.hasOwnProperty(i)) {
+					row = Ti.UI.createTableViewRow({
+						height : 95,
+						myTitle : rows[i].title,
+						data : rows[i],
+						className : 'productRow',
+						backgroundImage : '/images/TableViewRowBG.jpg',
+						selectedBackgroundImage : '/images/TableViewRowSelectedBG.png'
+					});
 
-                    img = Ti.UI.createImageView({
-                        image : Ti.App.APIURL + 'api/pic/product/' + rows[i].id + '/100/100/1',
-                        width : 85,
-                        height : 85,
-                        right : 10,
-                        borderRadius : 45,
-                        defaultImage : '/images/default.png'
-                    });
-                    row.add(img);
+					img = Ti.UI.createImageView({
+						image : Ti.App.APIURL + 'api/pic/product/' + rows[i].id + '/100/100/1',
+						width : 85,
+						height : 85,
+						right : 10,
+						borderRadius : 45,
+						defaultImage : '/images/default.png'
+					});
+					row.add(img);
 
-                    arrow = Ti.UI.createImageView({
-                        image : '/images/icon_7.png',
-                        top : 6,
-                        left : 0,
-                        width : 23,
-                        height : 79
-                    });
-                    row.add(arrow);
+					arrow = Ti.UI.createImageView({
+						image : '/images/icon_7.png',
+						top : 6,
+						left : 0,
+						width : 23,
+						height : 79
+					});
+					row.add(arrow);
 
-                    titleLbl = Ti.UI.createLabel({
-                        text : rows[i].title,
-                        left : 0,
-                        right : 110,
-                        top : 10,
-                        textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
-                        color : '#ffffff',
-                        font : {
-                            fontFamily : 'Arial',
-                            fontSize : 17,
-                            fontWeight : 'bold'
-                        }
-                    });
-                    row.add(titleLbl);
+					titleLbl = Ti.UI.createLabel({
+						text : rows[i].title,
+						left : 0,
+						right : 110,
+						top : 10,
+						textAlign : Ti.UI.TEXT_ALIGNMENT_RIGHT,
+						color : '#ffffff',
+						font : {
+							fontFamily : 'Arial',
+							fontSize : 17,
+							fontWeight : 'bold'
+						}
+					});
+					row.add(titleLbl);
 
-                    if (rows[i].price_shown_coupon < rows[i].price) {
+					if (rows[i].price_shown_coupon < rows[i].price) {
 
-                        pricebackground = Ti.UI.createView({
-                            backgroundImage : '/images/sss.png',
-                            bottom : 11,
-                            right : 110,
-                            width : 170,
-                            height : 38
-                        });
-                        row.add(pricebackground);
+						pricebackground = Ti.UI.createView({
+							backgroundImage : '/images/sss.png',
+							bottom : 11,
+							right : 110,
+							width : 170,
+							height : 38
+						});
+						row.add(pricebackground);
 
-                        pricebackground.add(Ti.UI.createLabel({
-                            text : parseFloat(rows[i].price).toFixed(2),
-                            left : 0,
-                            top : 0,
-                            width : 85,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-                            color : '#ffffff',
-                            font : {
-                                fontSize : 17,
-                                fontWeight : 'bold'
-                            }
-                        }));
+						pricebackground.add(Ti.UI.createLabel({
+							text : parseFloat(rows[i].price).toFixed(2),
+							left : 0,
+							top : 0,
+							width : 85,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+							color : '#ffffff',
+							font : {
+								fontSize : 17,
+								fontWeight : 'bold'
+							}
+						}));
 
-                        pricebackground.add(Ti.UI.createLabel({
-                            text : Ti.App.Properties.getString('currencyName', 'ريال سعودي'),
-                            left : 0,
-                            bottom : -5,
-                            width : 85,
-                            height : 38,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-                            color : '#ffffff',
-                            font : {
-                                fontSize : 14
-                            }
-                        }));
+						pricebackground.add(Ti.UI.createLabel({
+							text : Ti.App.Properties.getString('currencyName', 'ريال سعودي'),
+							left : 0,
+							bottom : -5,
+							width : 85,
+							height : 38,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+							color : '#ffffff',
+							font : {
+								fontSize : 14
+							}
+						}));
 
-                        pricebackground.add(Ti.UI.createLabel({
-                            text : parseFloat(rows[i].price_shown_coupon).toFixed(2),
-                            right : 0,
-                            top : 0,
-                            width : 85,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-                            color : '#000000',
-                            font : {
-                                fontSize : 17,
-                                fontWeight : 'bold'
-                            }
-                        }));
+						pricebackground.add(Ti.UI.createLabel({
+							text : parseFloat(rows[i].price_shown_coupon).toFixed(2),
+							right : 0,
+							top : 0,
+							width : 85,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+							color : '#000000',
+							font : {
+								fontSize : 17,
+								fontWeight : 'bold'
+							}
+						}));
 
-                        pricebackground.add(Ti.UI.createLabel({
-                            text : Ti.App.Properties.getString('currencyName', 'ريال سعودي'),
-                            right : 0,
-                            bottom : -5,
-                            width : 85,
-                            height : 38,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-                            color : '#000000',
-                            font : {
-                                fontSize : 14
-                            }
-                        }));
-                    } else {
+						pricebackground.add(Ti.UI.createLabel({
+							text : Ti.App.Properties.getString('currencyName', 'ريال سعودي'),
+							right : 0,
+							bottom : -5,
+							width : 85,
+							height : 38,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+							color : '#000000',
+							font : {
+								fontSize : 14
+							}
+						}));
+					} else {
 
-                        pricebackground = Ti.UI.createView({
-                            backgroundImage : '/images/bg_price_2.png',
-                            bottom : 11,
-                            right : 110,
-                            width : 83,
-                            height : 38
-                        });
-                        row.add(pricebackground);
+						pricebackground = Ti.UI.createView({
+							backgroundImage : '/images/bg_price_2.png',
+							bottom : 11,
+							right : 110,
+							width : 83,
+							height : 38
+						});
+						row.add(pricebackground);
 
-                        pricebackground.add(Ti.UI.createLabel({
-                            text : parseFloat(rows[i].price).toFixed(2),
-                            left : 0,
-                            top : 0,
-                            width : 85,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-                            color : '#000000',
-                            font : {
-                                fontSize : 17,
-                                fontWeight : 'bold'
-                            }
-                        }));
+						pricebackground.add(Ti.UI.createLabel({
+							text : parseFloat(rows[i].price).toFixed(2),
+							left : 0,
+							top : 0,
+							width : 85,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+							color : '#000000',
+							font : {
+								fontSize : 17,
+								fontWeight : 'bold'
+							}
+						}));
 
-                        pricebackground.add(Ti.UI.createLabel({
-                            text : Ti.App.Properties.getString('currencyName', 'ريال سعودي'),
-                            left : 0,
-                            bottom : -5,
-                            width : 85,
-                            height : 38,
-                            textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
-                            color : '#000000',
-                            font : {
-                                fontSize : 14
-                            }
-                        }));
-                    }
+						pricebackground.add(Ti.UI.createLabel({
+							text : Ti.App.Properties.getString('currencyName', 'ريال سعودي'),
+							left : 0,
+							bottom : -5,
+							width : 85,
+							height : 38,
+							textAlign : Ti.UI.TEXT_ALIGNMENT_CENTER,
+							color : '#000000',
+							font : {
+								fontSize : 14
+							}
+						}));
+					}
 
-                    tableRows.push(row);
-                }
-            }
+					tableRows.push(row);
+				}
+			}
 
-            tableView.fireEvent('reloadData', {
-                rows : tableRows
-            });
-        };
+			tableView.fireEvent('reloadData', {
+				rows : tableRows
+			});
+		};
 
-        xhr.send();
-    }
+		xhr.send();
+	}
 
-    var searchBar = Ti.UI.createSearchBar();
+	var searchBar = Ti.UI.createSearchBar();
 
-    tableView = Ti.UI.createTableView({
-        filterAttribute : 'myTitle',
-        //search : searchBar,
-        backgroundColor : 'transparent',
-        separatorColor : 'transparent'
-    });
+	tableView = Ti.UI.createTableView({
+		filterAttribute : 'myTitle',
+		//search : searchBar,
+		backgroundColor : 'transparent',
+		separatorColor : 'transparent'
+	});
 
-    tableView.addEventListener('runLoading', function() {
-        this.setData([{
-            title : 'جاري التحميل ....',
-            color : '#ffffff'
-        }]);
-    });
-    tableView.addEventListener('reloadData', function(e) {
-        this.setData(e.rows.length > 0 ? e.rows : [{
-            title : 'مشكلة تحميل، حاول بعد قليل.',
-            color : '#ffffff'
-        }]);
-        searchBar.blur();
-    });
-    filterData();
+	tableView.addEventListener('runLoading', function() {
+		this.setData([{
+			title : 'جاري التحميل ....',
+			color : '#ffffff'
+		}]);
+	});
+	tableView.addEventListener('reloadData', function(e) {
+		this.setData(e.rows.length > 0 ? e.rows : [{
+			title : 'مشكلة تحميل، حاول بعد قليل.',
+			color : '#ffffff'
+		}]);
+		searchBar.blur();
+	});
 
-    tableView.addEventListener('click', function(e) {
+	self.addEventListener('focus', filterData);
 
-        if (e.rowData.data) {
+	tableView.addEventListener('click', function(e) {
 
-            Ti.App.myTabGroup.setActiveTab(Ti.App.catalogTab);
+		if (e.rowData.data) {
 
-            Ti.App.fireEvent('openProductWindow', {
-                data : e.rowData.data
-            });
-        }
-    });
+			Ti.App.myTabGroup.setActiveTab(Ti.App.catalogTab);
 
-    self.add(tableView);
+			Ti.App.fireEvent('openProductWindow', {
+				data : e.rowData.data
+			});
+		}
+	});
 
-    return self;
+	self.add(tableView);
+
+	return self;
 }
 
 module.exports = offersListWin;
